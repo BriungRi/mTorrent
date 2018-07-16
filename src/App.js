8718af6cf6f8eaaf7d28d5014d27d3ea12bbca13
BriungRi <brian.li@mongodb.com>
2018-07-16 17:08:36 -0400

    Init

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import List from "./components/List.js";
+import Upload from "./components/Upload.js";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React/Electron</h2>
-        </div>
-        <p className="App-intro">
-          Hello Electron!
-        </p>
+        <p>Hello World</p>
+        {/* <List />
+        <Upload /> */}
       </div>
     );
   }
---
+++ src/components/Upload.js
@@ -0,0 +1,9 @@
+import {React, Component} from 'react'
+
+class Upload extends Component {
+    render() {
+        return(<div></div>)
+    }
+}
+
+export default Upload;
