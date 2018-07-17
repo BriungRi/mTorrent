9a1e34274d7d3774e0deb418407d33b325e5e7fe
BriungRi <brian.li@mongodb.com>
2018-07-17 11:34:18 -0400

    add node function

--- src/wrangler/routes/index.js
+++ src/wrangler/routes/index.js
@@ -1,5 +1,9 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const mongoDriver = require("mongodb");
+const mongoClient = mongoDriver.MongoClient;
+const replSet = mongoDriver.replSet;
+
+const router = express.Router();
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
@@ -7,15 +11,30 @@ router.get("/", function(req, res, next) {
 });
 
 router.post("/add_node", function(req, res, next) {
-  console.log("adding node");
-  console.log(req.body);
-  console.log(req.body.hi);
-  res.send("ok");
+  addNode(req.body.filename, req.body.mongo);
 });
 
 router.post("/remove_nodes", function(req, res, next) {
   console.log("removing nodes");
   res.send("ok");
 });
+
+function addNode(replSetName, mongoURL) {
+  const dbName = "admin";
+  const collectionName = "file";
+
+  MongoClient.connect(
+    replSetName,
+    function(err, db) {
+      var adminDb = db.admin();
+      adminDb.command({ replSetGetConfig: 1 }, function(err, conf) {
+        conf.members.push(mongoURL);
+        adminDb.command({ replSetGetConfig: conf }, function(err, info) {
+          console.log(info);
+        });
+      });
+    }
+  );
+}
 
 module.exports = router;
