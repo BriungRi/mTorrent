4032a66320472a35e76b061840d5a9e289944469
brianli <brnli7@gmail.com>
2018-07-16 23:00:39 -0400

    Basic UI for mTorrent

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import React, { Component } from "react";
+import Dropzone from "react-dropzone";
+import Logo from "./components/Logo.js";
 import List from "./components/List.js";
 import Upload from "./components/Upload.js";
+import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = { files: [] };
+
+    this.onDrop = this.onDrop.bind(this);
+  }
+
+  onDrop(files) {
+    this.setState({
+      files
+    });
+    console.log(files);
+  }
+
   render() {
     return (
       <div className="App">
-        <p>Hello World</p>
-        {/* <List />
-        <Upload /> */}
+        <Dropzone className="Dropzone" onDrop={this.onDrop} disableClick="true">
+          <Logo />
+          <Upload />
+          <List />
+        </Dropzone>
       </div>
     );
   }
--- src/components/List.js
+++ src/components/List.js
@@ -1,9 +1,46 @@
-import {React, Component} from 'react'
+import React, { Component } from "react";
+import "./../style/List.css";
+import cloud_download from "./../assets/cloud_download.png";
 
 class List extends Component {
-    render() {
-        return(<div></div>)
-    }
+  render() {
+    return (
+      <div>
+        <table className="Table">
+          <tr>
+            <th className="FilenameHeader">Filename</th>
+            <th className="SeedHeader">Seeds</th>
+            <th className="DownloadHeader" />
+          </tr>
+          <tr>
+            <td>music.mp3</td>
+            <td>23</td>
+            <td className="DownloadElement">
+              <img
+                className="DownloadImage"
+                src={cloud_download}
+                alt="Download"
+              />
+            </td>
+          </tr>
+          <tr>
+            <td>movie.mp4</td>
+            <td>30</td>
+            <td className="DownloadElement">
+              <img src={cloud_download} alt="Download" />
+            </td>
+          </tr>
+          <tr>
+            <td>files.zip</td>
+            <td>9</td>
+            <td className="DownloadElement">
+              <img src={cloud_download} alt="Download" />
+            </td>
+          </tr>
+        </table>
+      </div>
+    );
+  }
 }
 
 export default List;
