5b0dbaf181d363732f47d54b762ae60502905c0d
BriungRi <brian.li@mongodb.com>
2018-07-17 10:21:23 -0400

    Added some routes and mongodb node driver

--- src/wrangler/routes/index.js
+++ src/wrangler/routes/index.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/add_node', function(req, res, next) {
+
+});
+
+router.post('/remove_nodes', function(req, res, next) {
+
+});
+
 module.exports = router;
